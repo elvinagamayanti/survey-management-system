7f7a0d5943412dacdea45920bfacc2c1ab5d318e
adindaspm <222111844@stis.ac.id>
2025-01-10 08:36:09 +0700

    perbarui sidebar

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -2,10 +2,16 @@
 module.exports = {
   content: ["../resources/templates/**/*.{html,js}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: '#002B6A',
-      secondary: '#0093DD',
+    extend: {
+      colors: {
+        primary: '#008ed6',
+        secondary: '#0aadff',
+        'tahiti': {
+          light: '#67e8f9',
+          DEFAULT: '#06b6d4',
+          dark: '#0e7490',
+        },
+      },
     },
   },
   plugins: [],
